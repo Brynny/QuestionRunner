41bb79fcc9459011789d7894027108df9ffa721c
Brynny <111567@student.sae.edu.au>
2016-07-07 20:32:54 +0800

    Updated player model
    Added the new player model to the game, added a main menu button to the
    times table level, created the basic mathematics level with a background
    and player.

---
+++ Assets/Scripts/TimesTable/GoToMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToMainMenu : MonoBehaviour 
+
+{
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
