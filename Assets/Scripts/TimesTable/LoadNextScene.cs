e410f0c08f2115f19124994a4125c9a3bfbd380f
Brynny <111567@student.sae.edu.au>
2016-07-07 16:05:41 +0800

    Added Tree and reload level when completed

---
+++ Assets/Scripts/TimesTable/LoadNextScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour 
+{
+
+
+	public void LoadSameLevel () 
+	{
+		SceneManager.LoadScene ("TimesTable");
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			LoadSameLevel();
+			Destroy (other.gameObject);
+		}
+	}
+}
