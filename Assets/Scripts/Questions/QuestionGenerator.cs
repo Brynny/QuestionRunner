5c4778a787f9eaddacf39c0ece7bcb146d5a23b7
Brynny <111567@student.sae.edu.au>
2016-08-01 17:28:49 +0800

    Changed in game UI completely. Added new title

--- Assets/Scripts/Questions/QuestionGenerator.cs
+++ Assets/Scripts/Questions/QuestionGenerator.cs
@@ -8,7 +8,10 @@ public class QuestionGenerator : MonoBehaviour
 	public GameObject[] questions;
 	GameObject questionSpawned;
 	public bool questionAvailable;
-
+//	public int questionSetOne, questionSetTwo, questionSetThree;
+//	public enum questionSet {SETONE, SETTWO, SETTHREE};
+//	public questionSet activeSet;
+//
 	float timer = 5f;
 
 	void Update () 
@@ -28,6 +31,15 @@ public class QuestionGenerator : MonoBehaviour
 
 	public void SpawnQuestions()
 	{
+//		int questionIndex = 0;
+//		if (activeSet == questionSet.SETONE) {
+//			questionIndex = Random.Range (0, questionSetOne);
+//		} else if (activeSet == questionSet.SETTWO) {
+//			questionIndex = Random.Range (0, questionSetTwo);
+//		} else if (activeSet == questionSet.SETTHREE) {
+//			questionIndex = Random.Range (0, questionSetThree);
+//		}
+
 		int questionIndex = Random.Range (0, questions.Length);
 		questionSpawned = Instantiate (questions[questionIndex], spawnPoint.position, Quaternion.identity) as GameObject;
 		questionSpawned.transform.parent = GameObject.Find("Canvas").transform;
