73214b0034f811990e2105a8a8a072c9955d6a9b
Brynny <111567@student.sae.edu.au>
2016-08-15 13:13:06 +0800

    Added Tutorial Modes for 200MSprint and CityRunner

---
+++ Assets/Scripts/200MSprint/Tutorial200M.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tutorial200M : MonoBehaviour 
+{
+	public Text tutorialText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
---
+++ Assets/Tutorial/TutorialController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TutorialController : MonoBehaviour 
+{
+	public void Load200MSprintTutorial()
+	{
+		SceneManager.LoadScene ("Tutorial200M");
+	}
+
+	public void LoadCityRunnerTutorial()
+	{
+		SceneManager.LoadScene ("TutorialCityRunner");
+	}
+}
