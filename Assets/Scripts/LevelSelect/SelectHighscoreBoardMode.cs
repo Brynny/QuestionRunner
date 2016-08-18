690dfc8388507434afa6879d8aa8caf44b75bab5
Brynny <111567@student.sae.edu.au>
2016-08-18 17:12:00 +0800

    added audio to various scenes and updated the credits scene

---
+++ Assets/Scripts/LevelSelect/SelectHighscoreBoardMode.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SelectHighscoreBoardMode : MonoBehaviour
+{
+	public void HighscoreBoardLevel()
+	{
+		SceneManager.LoadScene("HighscoreLevelOne");
+	}
+}
