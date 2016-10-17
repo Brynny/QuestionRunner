931f475d334bb67fa8991cab3b3b47dcb601d734
Brynny <111567@student.sae.edu.au>
2016-10-17 14:15:46 +0800

    Added Character Select Screen
    Character in game doesnt work.
    Need to use old character

---
+++ Assets/Scripts/CharacterSelect/CharacterCreation.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CharacterCreation : MonoBehaviour 
+{
+
+	private GameObject[] characterList;
+	private int index;
+
+	private void Start()
+	{
+		index = PlayerPrefs.GetInt("CharacterSelected");
+
+		characterList = new GameObject[transform.childCount];
+
+		// fill array with character models
+		for (int i = 0; i <transform.childCount; i++)
+		{
+			characterList[i] = transform.GetChild(i).gameObject;
+		}
+
+		// toggle off their renderer
+		foreach (GameObject go in characterList)
+		{
+			go.SetActive(false);
+		}
+
+		// toggle on selected character
+		if (characterList[index])
+		{
+			characterList[index].SetActive(true);
+		}
+
+	}
+
+	public void ToggleLeft()
+	{
+		// Toggle off current model
+		characterList[index].SetActive(false);
+
+		index--; 
+		if(index < 0)
+			index = characterList.Length - 1;
+
+		//Toggle on current model
+		characterList[index].SetActive(true);
+	}
+
+	public void ToggleRight()
+	{
+		// Toggle off current model
+		characterList[index].SetActive(false);
+
+		index++; 
+		if(index == characterList.Length)
+			index = 0;
+
+		//Toggle on current model
+		characterList[index].SetActive(true);
+	}
+
+	public void StartGame()
+	{
+		PlayerPrefs.SetInt("CharacterSelected", index);
+		SceneManager.LoadScene("LevelSelect");
+	}
+
+
+
+
+
+
+
+}
+
+
